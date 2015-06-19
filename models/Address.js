e099f624cd4b458b5d3afa444052301ef21ccc84
Tolleiv Nietsch <info@tolleiv.de>
2015-06-19 23:38:13 +0200

    Fix JSCS lint checks

--- models/Address.js
+++ models/Address.js
@@ -1,14 +1,14 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Address = sequelize.define('Address', {
-    value: DataTypes.STRING,
-    comment: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return Address;
+module.exports = function (sequelize, DataTypes) {
+    var Address = sequelize.define('Address', {
+        value: DataTypes.STRING,
+        comment: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return Address;
 };
--- routes/ip.js
+++ routes/ip.js
@@ -2,13 +2,15 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
-
 router.get('/', function (req, res) {
     models.Address.find({
         where: {value: req.body.ip}
     }).then(function (address) {
-        if (!address) res.status(404).send("free");
-        else res.send("used");
+        if (!address) {
+            res.status(404).send('free');
+        } else {
+            res.send('used');
+        }
     });
 });
 
@@ -17,7 +19,7 @@ router.get('/used', function (req, res) {
         var ips = rows.map(function (row) {
             return row.value;
         });
-        res.send(ips.join(("\n")))
+        res.send(ips.join(('\n')))
     });
 });
 
@@ -25,20 +27,22 @@ router.put('/', function (req, res) {
     models.Address.create({
         value: req.body.ip
     }).then(function () {
-        res.send("success");
+        res.send('success');
     }).error(function () {
-        res.send("failure");
+        res.send('failure');
     });
 });
 router.delete('/', function (req, res) {
     models.Address.find({
         where: {value: req.body.ip}
     }).then(function (address) {
-        if (!address) res.status(404).send("not found");
-        else
+        if (!address) {
+            res.status(404).send('not found');
+        } else {
             address.destroy().then(function () {
-                res.send("success");
+                res.send('success');
             });
+        }
     });
 });
 
