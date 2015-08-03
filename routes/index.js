8d7754c1cfdfd1177208bd3dff56db04a4f16d9a
Tolleiv Nietsch <info@tolleiv.de>
2015-08-03 18:27:56 +0200

    Add some documentation content to the index page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    var port = process.env.PORT || '3000';
+    var uri = process.env.BASE_URI ? process.env.BASE_URI : 'localhost:' + port;
+    res.render('index', { base_uri: uri });
 });
 
 module.exports = router;
